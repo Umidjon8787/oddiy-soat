f6bf4d17594512391c841a38cc5486963a3a4675
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-04-18 17:02:11 +0300

    Initial commit

---
+++ Js/script.js
@@ -0,0 +1,30 @@
+
+
+function showTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    
+    if(hours == 0){
+        hours = 24;
+    }
+
+    if (hours > 24) {
+        hours = hours - 24;
+    }
+
+    hours = (hours < 10) ? "0" + hours:hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    
+    let time = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("ClockDisplay").innerText = time;
+
+    document.getElementById("ClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
